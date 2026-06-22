dab2fc4a8f2bbd6904101d607901dfc22adcaaa9
ahmedabwnhylh45-lab <ahmedabwnhylh45@gmail.com>
2026-06-22 18:39:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
     sessionName: "session",
     chanelid: "-1003652947211", // آيدي القناة
     chatgrupid: "-1003637772522", // آيدي الشات
-    thumburl: "https://telegra.ph/file/dcab8c983cc40f5a7092c.jpg" // رابط صورة ترحيبية شغال وصحيح
+    thumburl: "" // تم تفريغ الرابط لإيقاف خطأ الصورة
 };
