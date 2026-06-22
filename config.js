26c59a278f7563ade72c1add1fabf31b2c14a69d
ahmedabwnhylh45-lab <ahmedabwnhylh45@gmail.com>
2026-06-22 16:28:14 +0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     ownerId: 666,//change your id telegram 
-    telegramBotToken: "",///the bot is being administered in your channel
+    telegramBotToken: "8284447676:AAGYh1VB8vmEiOnUHEBbMVU9b2lSEbLLCVQ",///the bot is being administered in your channel
     sessionName: "session",
     chanelid: "@Xzesoinc13",///change your username channel 
     chatgrupid: "@",///change your grup public,do not grup private 
