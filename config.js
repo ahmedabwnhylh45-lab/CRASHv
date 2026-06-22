195d968a78292ddab560dadab780ac971b150833
ahmedabwnhylh45-lab <ahmedabwnhylh45@gmail.com>
2026-06-23 02:30:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    ownerId: 666,//change your id telegram 
-    telegramBotToken: "",///the bot is being administered in your channel
+    ownerId: 7027789447,//change your id telegram 
+    telegramBotToken: "8284447676:AAGYh1VB8vmEiOnUHEBbMVU9b2lSEbLLCVQ",///the bot is being administered in your channel
     sessionName: "session",
     chanelid: "@Notfound_Comander",///change your username channel 
     chatgrupid: "@NOTfound_COMANDERr",///change your grup public,do not grup private 
