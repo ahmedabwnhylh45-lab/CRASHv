334b71493231628ec1e77c9b947effd2c163a86f
ahmedabwnhylh45-lab <ahmedabwnhylh45@gmail.com>
2026-06-23 02:27:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  ownerId: "7027789447", // ضع الأرقام الخاصة بك
-  telegramBotToken: "8284447676:AAGYh1VB8vmEiOnUHEBbMVU9b2lSEbLLCVQ", // ضع التوكين الخاص بك
-  sessionName: "session",
-  CHANNEL_ID: "-1003652947211", // تأكد من كتابتها بهذا الشكل (حروف كبيرة)
-  chatgrupid: "-1003637772522",
-  thumburl: "" 
+    ownerId: 666,//change your id telegram 
+    telegramBotToken: "",///the bot is being administered in your channel
+    sessionName: "session",
+    chanelid: "@Notfound_Comander",///change your username channel 
+    chatgrupid: "@NOTfound_COMANDERr",///change your grup public,do not grup private 
+    thumburl: "https:///p_374885vzt1.jpg"
+};
 };
