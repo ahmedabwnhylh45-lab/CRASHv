6be8c2b53ba339f007580fb79fce352dd886cb8b
ahmedabwnhylh45-lab <ahmedabwnhylh45@gmail.com>
2026-06-22 16:19:56 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ownerId: 666,//change your id telegram 
+    telegramBotToken: "",///the bot is being administered in your channel
+    sessionName: "session",
+    chanelid: "@Xzesoinc13",///change your username channel 
+    chatgrupid: "@",///change your grup public,do not grup private 
+    thumburl: "https:///p_374885vzt1.jpg"
+};
