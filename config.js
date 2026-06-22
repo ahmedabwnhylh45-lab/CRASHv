40d5188bc840e2af855b4089e75df946f76b893f
ahmedabwnhylh45-lab <ahmedabwnhylh45@gmail.com>
2026-06-23 01:38:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    ownerId: 7027789447, // معرف صاحب البوت
-    telegramBotToken: "8284447676:AAGYh1VB8vmEiOnUHEBbMVU9b2lSEbLLCVQ", // توكن البوت
-    sessionName: "session",
-    chanelid: "-1003652947211", // آيدي القناة
-    chatgrupid: "-1003637772522", // آيدي الشات
-    thumburl: "" // تم تفريغ الرابط لإيقاف خطأ الصورة
+  ownerId: "7027789447", // ضع الأرقام الخاصة بك
+  telegramBotToken: "8284447676:AAGYh1VB8vmEiOnUHEBbMVU9b2lSEbLLCVQ", // ضع التوكين الخاص بك
+  sessionName: "session",
+  CHANNEL_ID: "-1003652947211", // تأكد من كتابتها بهذا الشكل (حروف كبيرة)
+  chatgrupid: "-1003637772522",
+  thumburl: "" 
 };
