6084307dcf96b9e340ad590b17a4a75db2494641
ahmedabwnhylh45-lab <ahmedabwnhylh45@gmail.com>
2026-06-30 22:31:13 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ownerId: 7027789447,//change your id telegram 
-    telegramBotToken: "8530000625:AAFM6O3z-8N8PUy1AiqZNCskmwDw4Ji7rFU",///the bot is being administered in your channel
-    sessionName: "session",
-    chanelid: "@Notfound_Comander",///change your username channel 
-    chatgrupid: "@NOTfound_COMANDERr",///change your grup public,do not grup private 
-    thumburl: "https://k.top4top.io/p_3785nf0ym1.jpg"
-};
